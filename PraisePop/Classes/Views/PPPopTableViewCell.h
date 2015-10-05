2e4419ff8adc6a6666370543a3b55d5c33b7f91a
Rudd Fawcett <rudd.fawcett@gmail.com>
2015-10-05 15:48:29 -0400

    Added pop xib and cell class

---
+++ PraisePop/Classes/Views/PPPopTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  PPPopTableViewCell.h
+//  PraisePop
+//
+//  Created by Rudd Fawcett on 10/5/15.
+//  Copyright © 2015 PraisePop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PPPopTableViewCell : UITableViewCell
+
+@end
---
+++ PraisePop/Classes/Views/PPPopTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  PPPopTableViewCell.m
+//  PraisePop
+//
+//  Created by Rudd Fawcett on 10/5/15.
+//  Copyright © 2015 PraisePop. All rights reserved.
+//
+
+#import "PPPopTableViewCell.h"
+
+@implementation PPPopTableViewCell
+
+- (void)awakeFromNib {
+    
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
