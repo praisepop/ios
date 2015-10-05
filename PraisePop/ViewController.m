f57b123ca99684ba5178df7995b91ef9396c3af2
Rudd Fawcett <rudd.fawcett@gmail.com>
2015-10-04 20:26:25 -0400

    Initial Commit

---
+++ PraisePop/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PraisePop
+//
+//  Created by Rudd Fawcett on 10/4/15.
+//  Copyright © 2015 PraisePop. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ PraisePop/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PraisePop
+//
+//  Created by Rudd Fawcett on 10/4/15.
+//  Copyright © 2015 PraisePop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
