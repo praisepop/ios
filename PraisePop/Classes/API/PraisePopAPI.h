36128d39660129b0068e11837cda0426d9000c0b
Rudd Fawcett <rudd.fawcett@gmail.com>
2015-10-25 15:44:44 -0400

    Started hashing out API

--- PraisePop/Classes/API/PraisePopAPI.h
+++ PraisePop/Classes/API/PraisePopAPI.h
@@ -6,9 +6,12 @@
 //  Copyright © 2015 PraisePop. All rights reserved.
 //
 
-#import <AFNetworking/AFNetworking.h>
+#import <Overcoat/Overcoat.h>
+#import <SSKeychain/SSKeychain.h>
 
 #import <UIKit/UIKit.h>
+
+@class PAAuthentication;
 
 @interface PraisePopAPI : AFHTTPRequestOperationManager
 
@@ -16,8 +19,8 @@
 
 //- (void)signup:(NSDictionary *)attributes withPassword:(NSString *)password success:(void (^)(NSDictionary *result))success failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure;
 
-- (void)login:(NSString *)email withPassword:(NSString *)password success:(void (^)(NSDictionary *result))success failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure;
+- (void)login:(NSString *)email withPassword:(NSString *)password success:(void (^)(BOOL result))success failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure;
 
-- (void)posts:(void (^)(NSArray *posts))success failure:(void (^)(AFHTTPRequestOperation *, NSError *))failure;
+- (void)posts:(void (^)(BOOL, NSArray *posts))success failure:(void (^)(AFHTTPRequestOperation *, NSError *))failure;
 
 @end
--- PraisePop/Classes/API/PraisePopAPI.m
+++ PraisePop/Classes/API/PraisePopAPI.m
@@ -8,13 +8,21 @@
 
 #import "PPUser.h"
 #import "PPOrganization.h"
-#import "PPUpvote.h"
 #import "PPPost.h"
+#import "PPAuthentication.h"
+#import "PPReaction.h"
 
 #import "PraisePopAPI.h"
 
-
 static NSString * const PraisePopAPIBaseURLString = @"http://localhost:8080/api/v1/";
+
+static CGFloat const PRAISE_POP_FEED_LIMIT = 25;
+
+@interface PraisePopAPI ()
+
+@property (nonatomic) NSUInteger currentPage;
+
+@end
 
 @implementation PraisePopAPI
 
@@ -29,50 +37,87 @@ static NSString * const PraisePopAPIBaseURLString = @"http://localhost:8080/api/
     return _sharedClient;
 }
 
++ (NSDictionary *)modelClassesByResourcePath {
+    return @{
+             @"users/authenticate" : [PPAuthentication class],
+             @"orgs/*/users/random" : [PPUser class],
+             @"orgs/*/posts" : [PPPost class]
+             };
+}
+
 - (NSIndexSet *)acceptableStatusCodes {
     NSMutableIndexSet *acceptedCodes = [[NSMutableIndexSet alloc]
                                         initWithIndexSet:self.responseSerializer.acceptableStatusCodes];
     
-    NSIndexSet *acceptable = [NSIndexSet.alloc initWithIndexesInRange:NSMakeRange(200, 600)];
+    NSIndexSet *acceptable = [NSIndexSet.alloc initWithIndexesInRange:NSMakeRange(100, 600)];
     
     [acceptedCodes addIndexes:acceptable];
     
     return acceptedCodes;
 }
 
-- (void)login:(NSString *)email withPassword:(NSString *)password success:(void (^)(NSDictionary *))success failure:(void (^)(AFHTTPRequestOperation *, NSError *))failure {
-    NSDictionary *paramters = @{@"email" : email, @"password" : password};
+- (void)login:(NSString *)email withPassword:(NSString *)password success:(void (^)(BOOL))success failure:(void (^)(AFHTTPRequestOperation *, NSError *))failure {
+    NSDictionary *paramters = @{
+                                @"email" : email,
+                                @"password" : password
+                                };
     
     [self POST:@"users/authenticate" parameters:paramters success:^(AFHTTPRequestOperation * _Nonnull operation, id  _Nonnull responseObject) {
-        if (responseObject[@"result"]) {
-            [[NSUserDefaults standardUserDefaults] setObject:responseObject[@"token"] forKey:@"token"];
+        NSDictionary *response = (NSDictionary *)responseObject;
+        
+        if (response[@"result"] && [response[@"result"] boolValue] == NO) {
+            success(NO);
         }
-    } failure:^(AFHTTPRequestOperation * _Nonnull operation, NSError * _Nonnull error) {
-        NSLog(@"%@", error.localizedDescription);
-    }];
+        else {
+            PPAuthentication *authentication = [MTLJSONAdapter modelOfClass:PPAuthentication.class fromJSONDictionary:response error:nil];
+            [PraisePop saveToken:authentication.token email:authentication.user.email];
+            [PraisePop saveOrganizations:authentication.user.organizations];
+            [PraisePop saveUserAccount:authentication.user];
+            success(YES);
+        }
+    } failure:failure];
 }
 
-- (void)posts:(void (^)(NSArray *))success failure:(void (^)(AFHTTPRequestOperation *, NSError *))failure {
-    NSDictionary *parameters = @{@"token":self.token};
-    
-    NSString *organizationId = @"561096efb6bc350000000003"; // MY TEMP ID
-    NSString *url = [NSString stringWithFormat:@"organization/%@/posts", organizationId];
-    
-    [self POST:url parameters:parameters success:^(AFHTTPRequestOperation * _Nonnull operation, id  _Nonnull responseObject) {
-        if (responseObject[@"data"]) {
-            NSMutableArray *posts;
-            
-            for (NSDictionary *post in responseObject[@"data"]) {
-                PPPost *aPost = [PPPost.alloc initWithAttributes:post];
-                
-                [posts addObject:aPost];
-            }
-            
-            success(posts);
+- (void)posts:(void (^)(BOOL, NSArray *))success failure:(void (^)(AFHTTPRequestOperation *, NSError *))failure {
+    NSString *path = [NSString stringWithFormat:@"orgs/%@/posts", PraisePop.parentOrganizationID];
+
+    NSDictionary *paramters = @{
+                            @"token" : PraisePop.userToken,
+                            @"limit" : @(PRAISE_POP_FEED_LIMIT),
+                            @"page" : @(1)
+                            };
+
+    [self GET:path parameters:paramters success:^(AFHTTPRequestOperation * _Nonnull operation, id  _Nonnull responseObject) {
+        NSDictionary *response = (NSDictionary *)responseObject;
+        
+        if (response[@"result"] && [response[@"result"] boolValue] == NO) {
+            success(NO, nil);
         }
-    } failure:^(AFHTTPRequestOperation * _Nonnull operation, NSError * _Nonnull error) {
-        NSLog(@"%@", error.localizedDescription);
-    }];
+        else {
+            NSLog(@"%@", response);
+        }
+    } failure:failure];
+    
+//    NSDictionary *parameters = @{@"token":self.token};
+    
+//    NSString *organizationId = @"561096efb6bc350000000003"; // MY TEMP ID
+//    NSString *url = [NSString stringWithFormat:@"organization/%@/posts", organizationId];
+//
+//    [self POST:url parameters:parameters success:^(AFHTTPRequestOperation * _Nonnull operation, id  _Nonnull responseObject) {
+//        if (responseObject[@"data"]) {
+//            NSMutableArray *posts;
+//            
+//            for (NSDictionary *post in responseObject[@"data"]) {
+//                PPPost *aPost = [PPPost.alloc initWithAttributes:post];
+//                
+//                [posts addObject:aPost];
+//            }
+//            
+//            success(posts);
+//        }
+//    } failure:^(AFHTTPRequestOperation * _Nonnull operation, NSError * _Nonnull error) {
+//        NSLog(@"%@", error.localizedDescription);
+//    }];
 }
 
 
